620906010b8095e202fd6f528802127534f72722
Christian Fuentes <christianfuentes@campus-114-112.ucdavis.edu>
2024-10-03 13:56:14 -0700

    week 2

---
+++ week 2.R
@@ -0,0 +1,85 @@
+
+
+
+
+
+
+
+
+
+
+
+#order of operations
+4 + (8 * 3) ^ 2
+# this is a comment
+
+#scientific notation
+ 2 / 100000
+ 4e3
+ 
+#mathematical functions
+ 
+ exp(1)
+ 
+ # r help files
+ log(2, 4)
+ log(x = 2, base =4)
+ x <- 1
+ 1^1
+ x
+ rm(x)
+ 
+ 
+ #nested functions
+ sqrt(exp(4))
+ log(exp(100))
+ 
+ #six comparison functions
+ mynumber <-6 
+ mynumber == 5
+ 
+ 
+ x^1
+ mynumber
+ 
+ library
+ 
+ mynumber != 5
+ 
+ mynumber > 4
+ my number < 3
+ mynumber >= 2
+ mynumber <= -1
+ #objects and assignment
+ mynumber <- 7
+ 
+ othernumber <- -3
+ mynumber * othernumber
+ #object name conventions
+ numSamples <- 50
+ num_samples <- 40
+ rm(num_samples)
+ 
+ #tab completion
+ rm(kist = ls())
+ 
+ #errors and warnings
+ log ("a_word") <- log("a_word")
+ log_of_word
+ 
+ log_of_negativelog_ <- log(-2)
+ 
+
+ #challenge
+ 2^2
+ 
+ elephant1_kg <-  3492 
+ elephant2_lb <-7757
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
