cb5d6a08a447203055281bdd1c460b4a15f96935
Christian Fuentes <christianfuentes@Christians-MacBook-Pro.local>
2024-10-16 18:42:03 -0700

    save week 3

--- Week 3 Homework.R
+++ Week 3 Homework.R
@@ -4,7 +4,7 @@ library(dplyr)
 select(surveys, species_id, weight, plot_id)
 colnames(surveys)
 surveys_base <- surveys[1:5000, c(6, 9, 13)]
-surveys_base <- surveys_base[complete.cases(surveys_base)
+surveys_base <- surveys_base[complete.cases(surveys_base),]
 surveys_base$species_id <- factor(surveys_base$species_id)                             
 surveys_base$plot_type <- factor(surveys_base$plot_type)
 levels(surveys_base$species_id)
