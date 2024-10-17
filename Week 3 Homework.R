19e525113ea0e34ef4369c3749ab5b589d30210c
Christian Fuentes <christianfuentes@Christians-MacBook-Pro.local>
2024-10-16 18:38:31 -0700

    Save week 3 homework

---
+++ Week 3 Homework.R
@@ -0,0 +1,10 @@
+surveys<- read.csv("data/portal_data_joined.csv")
+select(surveys, species_id, weight, plot_id)
+library(dplyr)
+select(surveys, species_id, weight, plot_id)
+colnames(surveys)
+surveys_base <- surveys[1:5000, c(6, 9, 13)]
+surveys_base <- surveys_base[complete.cases(surveys_base)
+surveys_base$species_id <- factor(surveys_base$species_id)                             
+surveys_base$plot_type <- factor(surveys_base$plot_type)
+levels(surveys_base$species_id)
