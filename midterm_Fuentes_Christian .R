ed1a15f37ece3fb16c12e564d2e96890a043b2da
Christian Fuentes <christianfuentes@campus-105-022.ucdavis.edu>
2024-11-07 12:40:27 -0800

    Midterm

--- midterm_Fuentes_Christian .R
+++ midterm_Fuentes_Christian .R
@@ -11,4 +11,15 @@ running_laps <- lap_dt %>%
   T ~ 'slow'),
   form = case_when(year == 2024 ~ 'new form',
   T ~ 'old form'))
-running_laps %>% group_by(form,pace_cat)
+running_laps %>% group_by(form,pace_cat) %>% 
+  summarize(avg_spm = mean(steps_per_minute)) %>%
+  pivot_wider(id_cols = form,values_from = avg_spm,names_from = pace_cat) %>%
+  select(form,slow,medium,fast)
+running_laps %>% filter(form == 'new form') %>%
+  mutate(months = ifelse(month %in% 1:6,'early 2024','late 2024')) %>%
+  group_by(months) %>% 
+  summarize(
+    min_spm = min(steps_per_minute),
+    median_spm = median(steps_per_minute),
+    mean_spm = mean(steps_per_minute),
+    max_spm = max(steps_per_minute))
